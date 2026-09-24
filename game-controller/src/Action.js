868f2079ccc7693d5fcb5e1d5b51369a3c611b94
Sephie <sp223tj@student.lnu.se>
2026-09-24 08:24:48 +0200

    Create Action.js

---
+++ game-controller/src/Action.js
@@ -0,0 +1,12 @@
+/**
+ * Represents an action that can be triggered by bindings.
+ */
+export class Action {
+
+    /**
+     * Creates new action.
+     */
+    constructor(name) {
+        this.#name = name
+    }
+}
