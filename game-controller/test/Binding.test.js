b22820698821244521fbe1b3b73dd31ab0b5d3a4
Sephie <sp223tj@student.lnu.se>
2026-09-24 08:06:09 +0200

    Successful test!

--- game-controller/test/Binding.test.js
+++ game-controller/test/Binding.test.js
@@ -3,4 +3,7 @@ import assert from 'node:assert/strict'
 import { Binding } from '../src/Binding.js'
 
 test('Binding stores input', () => {
+    const binding = new Binding('SPACE')
+
+    assert.equal(binding.getInput(), 'SPACE')
 })
