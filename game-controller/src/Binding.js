c98b563cd99d55c0d638368798f35d9c7f35ce75
Sephie <sp223tj@student.lnu.se>
2026-09-24 07:44:12 +0200

    Add getInput method and JSDoc.

--- game-controller/src/Binding.js
+++ game-controller/src/Binding.js
@@ -1,9 +1,29 @@
+/**
+ * Represents an input binding.
+ * 
+ * The binding stores the input it reprsents and
+ * if the input is pressed.
+ */
 export class Binding {
     #input
     #pressed
 
-    constructor() {
-        this.#input = this.#input
+    /**
+     * Creates a new binding.
+     * 
+     * @param {string} input - The input represented by this binding. 
+     */
+    constructor(input) {
+        this.#input = input
         this.#pressed = false
+    }
+
+    /**
+     * Returns the input represented by tihs binding.
+     * 
+     * @returns {string} - Bound input.
+     */
+    getInput() {
+        return this.#input
     }
 }
