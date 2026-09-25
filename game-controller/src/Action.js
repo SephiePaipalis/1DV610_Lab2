470ab905e1f2f32333a9da8112efb0b7618d45da
Sephie <sp223tj@student.lnu.se>
2026-09-25 21:20:06 +0200

    Update Action.js to allow bindings and create a test for it.

--- game-controller/src/Action.js
+++ game-controller/src/Action.js
@@ -1,12 +1,26 @@
+import { Binding } from './Binding.js'
+
 /**
  * Represents an action that can be triggered by bindings.
  */
 export class Action {
+    #name
+    #bindings
 
     /**
      * Creates new action.
      */
     constructor(name) {
         this.#name = name
+        this.#bindings = []
+    }
+
+    /**
+     * Returns name of action.
+     * 
+     * @returns {string} Action name.
+     */
+    getName() {
+        return this.#name
     }
 }
