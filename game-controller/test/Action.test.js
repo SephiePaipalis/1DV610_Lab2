1d3c06d64f0f8c8f08abe25e087ac64b579c8966
Sephie <sp223tj@student.lnu.se>
2026-09-25 21:37:53 +0200

    Add test for duplicate bindings

---
+++ game-controller/test/Action.test.js
@@ -0,0 +1,37 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import { Action } from '../src/Action.js'
+import { Binding } from '../src/Binding.js'
+
+test('Action stores name', () => {
+    const action = new Action('jump')
+
+    assert.equal(action.getName(), 'jump')
+})
+
+test ('Action starts without bindings', () => {
+    const action = new Action('jump')
+
+    assert.equal(action.getBindings().length, 0)
+})
+
+test ('Action can add a binding', () => {
+    const action = new Action('jump')
+    const binding = new Binding('SPACE')
+
+    action.addBinding(binding)
+
+    assert.equal(action.getBindings().length, 1)
+    assert.equal(action.getBindings()[0], binding)
+})
+
+test ('Action does not add same input twice', () => {
+    const action = new Action('jump')
+    const firstBinding = new Binding('SPACE')
+    const secondBinding = new Binding('SPACE')
+
+    action.addBinding(firstBinding)
+    action.addBinding(secondBinding)
+
+    assert.equal(action.getBindings().length, 1)
+})
