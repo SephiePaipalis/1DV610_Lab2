005e114feafe997ca31f86c3ee0f270e2dc2f908
Sephie <sp223tj@student.lnu.se>
2026-09-26 20:27:42 +0200

    Add getName/getActions methods in controlgroup

--- game-controller/src/ControlGroup.js
+++ game-controller/src/ControlGroup.js
@@ -5,7 +5,19 @@ export class ControlGroup {
 
     #name
 
+    /**
+     * Creates new control group
+     */
     constructor() {
         this.#name = this.#name
+    }
+
+    /**
+     * Returns name of the group.
+     * 
+     * @returns {string} - Group name.
+     */
+    getName() {
+        return this.#name
     }
 }
